f9851b4cd171093c82fbe7ba4c8edb42e946fe45
uhudhanAtes <uhudhanx@gmail.com>
2024-06-14 14:06:43 +0300

    redis update

--- src/executors/Thresholding.py
+++ src/executors/Thresholding.py
@@ -3,6 +3,7 @@ import cv2
 import sys
 import numpy as np
 import os
+from datetime import datetime
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 
 from sdks.novavision.src.base.component import Component
@@ -10,7 +11,7 @@ from components.Thresholding.src.models.PackageModel import PackageConfigs,Confi
 from sdks.novavision.src.base.response import Response
 from pydantic import ValidationError
 from sdks.novavision.src.media.image import Image
-from sdks.novavision.src.base.redis import RedisPubSubManager
+from sdks.novavision.src.base.redis import RedisWrapper
 from sdks.novavision.src.base.app import App
 
 
@@ -19,11 +20,16 @@ class Thresholding(Component):
         self.error_list = []
         try:
             super().__init__(request)
+            self.start_time = datetime.now()
             self.mode_debug = self.request.data.get("debug", False)
+            if App.get_app_mode() == 'runtime' and App.get_type_engine() == 'nv-mqtt':
+                if self.mode_debug == False:
+                    self.r = RedisWrapper()
+            else:
+                self.r = None
             self.request.model = PackageModel(**(self.request.data))
             self.type = self.request.get_param("configType")
             self.images = self.request.get_param("inputImage")
-            self.r = RedisPubSubManager()
             self.islist = Image.is_list(self.images)
             self.load_parameters()
 
@@ -51,22 +57,22 @@ class Thresholding(Component):
     def thresholding(self, image):
         image = np.asarray(image).astype(np.uint8)
 
-        if len(image.shape)==3:
+        if len(image.shape) == 3:
             image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         if self.type == "GlobalThresholding":
-            if self.global_type=="black white":
+            if self.global_type == "black white":
                 _, th_image = cv2.threshold(image, self.th_value, self.max_value, cv2.THRESH_BINARY)
-            elif self.global_type=="black white inv":
+            elif self.global_type == "black white inv":
                 _, th_image = cv2.threshold(image, self.th_value, self.max_value, cv2.THRESH_BINARY_INV)
-            elif self.global_type=="color like grey":
+            elif self.global_type == "color like grey":
                 _, th_image = cv2.threshold(image, self.th_value, self.max_value, cv2.THRESH_TRUNC)
-            elif self.global_type=="blackening":
+            elif self.global_type == "blackening":
                 _, th_image = cv2.threshold(image, self.th_value, self.max_value, cv2.THRESH_TOZERO)
-            elif self.global_type=="blackening inv":
+            elif self.global_type == "blackening inv":
                 _, th_image = cv2.threshold(image, self.th_value, self.max_value, cv2.THRESH_TOZERO_INV)
-            elif self.global_type=="auto thresholding":
-                _, th_image = cv2.threshold(image,0, self.max_value, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
+            elif self.global_type == "auto thresholding":
+                _, th_image = cv2.threshold(image, 0, self.max_value, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
 
         elif self.type == "LocalThresholding":
             if self.local_type == "mean":
@@ -84,9 +90,9 @@ class Thresholding(Component):
                 img.value = Image.encode64(np.asarray(self.thresholding(np.array(img.value))), img.mimeType)
                 image.append(img)
         else:
-            img = Image.get_image(self.images, self.mode_debug)
-            img.value = np.asarray(self.thresholding(np.array(img.value)))
-            image = Image.set_image(img, self.request.model.uID, self.mode_debug)
+            img = Image.get_image(img=self.images, redis_PubSub=self.r, mode_debug=self.mode_debug)
+            img.value = self.thresholding(img.value)
+            image = Image.set_image(img=img, redis_PubSub=self.r, package_uID=self.request.model.uID, mode_debug=self.mode_debug)
 
         outputImage = OutputImage(value=image)
         Outputs = ThresholdingOutputs(outputImage=outputImage)
@@ -96,11 +102,15 @@ class Thresholding(Component):
         packageConfigs = PackageConfigs(executor=executor)
         packageModel = PackageModel(configs=packageConfigs)
 
+        print(f"Start :", self.start_time.strftime("%Y-%m-%d %H:%M:%S:%f")[:-3])
+        self.now = datetime.now()
+        print(f"Stop :", self.now.strftime("%Y-%m-%d %H:%M:%S:%f")[:-3], '\n\n')
+
         if App.get_app_mode() == 'runtime' and App.get_type_engine() == 'nv-mqtt':
             if self.mode_debug == True:
                 return Response(model=packageModel, mode_debug=self.mode_debug).response()
             else:
-                Response(model=packageModel, mode_debug=self.mode_debug).response()
+                Response(model=packageModel, redis_PubSub=self.r, mode_debug=self.mode_debug).response()
         elif App.get_app_mode() == 'api' or (App.get_app_mode() == 'runtime' and App.get_type_engine() == 'nv-thrd'):
             return Response(model=packageModel, mode_debug=self.mode_debug).response()
         else:
@@ -108,4 +118,4 @@ class Thresholding(Component):
 
 
 if "__main__" == __name__:
-    r = RedisPubSubManager()._subscribe(room_id=sys.argv[1])
+    r = RedisWrapper()._subscribe(room_id=sys.argv[1])
